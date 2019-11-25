a1494a11aa3bc6a95fe64055ffe2a04077a7d75a
tahusseo <violet.stylo@laposte.net>
2019-11-25 19:28:43 +0100

    tweak mode nuit / cohérence couleurs / deplacement mode nuit dans menu

--- js/home-animations.js
+++ js/home-animations.js
@@ -1,46 +1,33 @@
 $(document).ready(function() {
 
-
-
-	//--------------------Jour / Nuit (checkbox)------------------
-	$(".switch input").prop("checked", false);	//Decoche la checkbox jour/nuit
-
-	$(".switch input").change(function() { //Check si la checkbox change d'état
-		$(".modeNuitTexte").toggleClass("active", 1000); //Change le texte jour / nuit
-		$(".background").toggleClass("active", 1000); //Change l'arriere plan
-		$(".navbar").toggleClass("active", 1000); //Change la barre de navigation
-	});
-
-
 	//-----------------------Fade-in logos---------------------------
-	$("img.logo").delay(200).animate({
-		opacity: 1,
-		width: "50%"}, 800);
-	$("img.separation").delay(500).animate({opacity: 1}, 1000);
+		$("img.logo").delay(200).animate({
+			opacity: 1,
+			width: "50%"}, 800);
+		$("img.separation").delay(500).animate({opacity: 1}, 1000);
 
 	//-----------------Scroll to anchor------------------------------
-	$("img.homeImg").click(function (){ //from anywhere to accueil
-		$("body, html").animate({
-			scrollTop: $("img.logo").offset().top -100
-		}, 600);
-	});	
-	$("img.separation").click(function (){ //from separation to presentation1
-		$("body, html").animate({
-			scrollTop: $("#presentation").offset().top -100
-		}, 800);
-	});
-
+		$("img.homeImg").click(function (){ //from anywhere to accueil
+			$("body, html").animate({
+				scrollTop: $("img.logo").offset().top -100
+			}, 600);
+		});	
+		$("img.separation").click(function (){ //from separation to presentation1
+			$("body, html").animate({
+				scrollTop: $("#presentation").offset().top -100
+			}, 800);
+		});
 });
 
 
 $(window).scroll(function() {
 
 	//Fade-out scroll
-	$("img.logo").css("opacity", 1 - $(window).scrollTop() / 400); //Logo_home
-	$("img.separation").css("opacity", 1 - $(window).scrollTop() / 700); //Separation fleche
+		$("img.logo").css("opacity", 1 - $(window).scrollTop() / 400); //Logo_home
+		$("img.separation").css("opacity", 1 - $(window).scrollTop() / 700); //Separation fleche
 
 	//Fade in-scroll
-	$("img.presentationImg").css("opacity", 0 + $(window).scrollTop() / 400); //Img Presentation
+		$("img.presentationImg").css("opacity", 0 + $(window).scrollTop() / 400); //Img Presentation
 
 });
 
--- js/navbar-animation.js
+++ js/navbar-animation.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	//Fermer searchPanel (perte de focus)
 	$(".search").blur(function() {
 		$(".searchPanel").toggleClass("active", 1000);
-		$(".decorationSearch").css({"opacity": 1}, 1000);
+		$(".decorationSearch").css({"opacity": 0}, 1000);
 	});
 
 
@@ -25,6 +25,48 @@ $(document).ready(function() {
 		$(".menuPanel").toggleClass("active", 1000);
 	});
 
+//--------------------------------------Jour / Nuit (checkbox)-----------------------------------
+	$(".switch input").prop("checked", false);	//Decoche la checkbox jour/nuit
 
+	$(".switch").click(function() {
 
+			//NIGHT MODE
+		if ($(".checkboxClass").is(':checked')) {
+			$("img.logo").css({"filter": "invert(1)"}); //logo inverted nuit
+			
+			$(".background").css({"background-image": "url(images/bg_home_nuit.png)"}); //background nuit
+			
+			$(".navbar").css({"background-color": "#3e4b61"}); // navbar nuit
+				$("img.homeImg").css({"filter": "invert(1)"}); //home inverted nuit
+				$("img.loupeImg").css({"filter": "invert(1)"}); //loupe inverted nuit
+				$("img.menuImg").css({"filter": "invert(1)"}); //menu inverted nuit
+				
+				$(".searchPanel").css({"background-color": "#3e4b61"}); //search box nuit
+
+					$(".search").css({"color": "#dae1ed"}); //search text nuit
+						$(".decorationSearch").css({"color": "#dae1ed"}); //search decoration nuit
+				
+				$(".menuPanel").css({"background-color": "#505f7a"}); //menu nuit
+					$(".switchText").text("Mode nuit: Activé"); //texte slider activé nuit
+
+			//DAY MODE
+		} else {
+			$("img.logo").css({"filter": "invert(0)"}); //logo normal jour
+			
+			$(".background").css({"background-image": "url(images/bg_home.png)"}); //background jour
+			
+			$(".navbar").css({"background-color": "#ebf5ff"}); //navbar jour
+				$("img.homeImg").css({"filter": "invert(0)"}); //home normal jour
+				$("img.loupeImg").css({"filter": "invert(0)"});//loupe normal jour
+				$("img.menuImg").css({"filter": "invert(0)"});//menu normal jour
+				
+				$(".searchPanel").css({"background-color": "#ebf5ff"}); //search box jour
+					$(".search").css({"color": "#3e4b61"}); //search text jour
+						$(".decorationSearch").css({"color": "#5f6f73"}); //search decoration jour
+				
+				$(".menuPanel").css({"background-color": "#f6ebf7"}); //menu jour
+					$(".switchText").text("Mode nuit: Désactivé"); //texte slider désactivé jour
+
+		}
+	});
 });
